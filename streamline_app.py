172cef210c9fdac19c955ddd5f63f3855abbc322
jjosss0 <josmalli09@gmail.com>
2025-10-29 10:54:58 -0400

    Comment out Streamlit elements and fix imports
    Comment out Streamlit title and write statements. Fix import statement for pandas and set page configuration.

--- streamline_app.py
+++ streamline_app.py
@@ -1,4 +1,13 @@
-import streamlit as st
+#import streamlit as st
 
-st.title('👋🏽welsome to my website')
-st.write("i'm building this live in class!")
+#st.title('👋🏽welsome to my website')
+#st.write("i'm building this live in class!")
+
+impor pandas as pd 
+from datetime import datetime
+
+#page config
+st.set_page_config
+page_title = 'joss | portafolio',
+page_icon='👻',
+layout= 'wide'
