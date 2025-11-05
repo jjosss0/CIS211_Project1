ab409837b2686ac424031340dd3b7e515fada66d
jjosss0 <josmalli09@gmail.com>
2025-11-05 11:13:07 -0500

    Remove space in About page condition

--- streamline_app.py
+++ streamline_app.py
@@ -61,7 +61,7 @@ if page == '🏡Home':
        st.image('https://github.com/jjosss0/CIS211_Project1/blob/bc25b2d3ff67041bde71e5a816270b8acd9d2ca9/brown-chihuahua-standing-in-grass-071723.jpg?raw=true', use_column_width=True)
 
     # About Page
-    elif page == '🤠 About':
+    elif page == '🤠About':
      st.title('About Me')
    
      # Timeline of my Professional Journey
