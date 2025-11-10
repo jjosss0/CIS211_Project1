02bafae8f6cdc2c3c954ca8f406a54f58dd3709f
jjosss0 <josmalli09@gmail.com>
2025-11-10 10:48:36 -0500

    Fix indentation and syntax in streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -82,12 +82,14 @@ elif page == '🤠About':
     for i, interest in enumerate(interests):
         with cols[i % 3]:
             st.info(f'🔷 {interest}')
-            elif page == '💼 Projects':
+ elif page == '💼 Projects':
   st.title('My Projects')
   st.write('Here are some projects I have worked on:')
-     # Project 1
+     
+
+    # Project 1
     with st.container():
-    col1, col2 = st.columns([1, 2])
+        col1, col2 = st.columns([1, 2])
   
     with col1:
         st.image('https://iprx-cms-content.ams1.vultrobjects.com/Blog_How_To_Crawl_4_capcha_ded9206d5f.png')
